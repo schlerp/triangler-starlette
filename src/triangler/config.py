7966112c27471a7a9a3d2e7d345acf76de69816c
PattyC <patrickcoffey91@gmail.com>
2024-09-27 11:42:24 +0930

    Add tests for `config.py` module
    Prior to this change, the config.py module was basically just a place
    holder file.
    
    This change updates that file to include some useful methods for getting
    config values from the environment:
    * `get_bool_from_env()`
    * `get_str_from_env()`
    
    This change also adds tests for the config module.

--- src/triangler/config.py
+++ src/triangler/config.py
@@ -1,1 +1,27 @@
-DEBUG = True
+import os
+
+
+def get_bool_from_env(env_var: str, default: bool | None = None) -> bool | None:
+    """
+    Get a boolean value from an environment variable. If the environment variable is not set,
+    return the default.
+
+
+    :param env_var: The environment variable to get the boolean value from.
+    :param default: The default value to return if the environment variable is not set.
+    :return: The boolean value of the environment variable or the default.
+    """
+    if env_var not in os.environ:
+        return default
+    return os.environ.get(env_var, "false").lower() in ("true", "t", "1", "yes", "y")
+
+
+def get_str_from_env(env_var: str, default: str | None = None) -> str | None:
+    if env_var not in os.environ:
+        return default
+    return os.environ.get(env_var)
+
+
+DEBUG = get_bool_from_env("DEBUG", False)
+
+SECRET_KEY = get_str_from_env("SECRET_KEY", "secret")
